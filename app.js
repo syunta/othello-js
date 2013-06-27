fd8266b5aba5b57a762bdcadb48153a6523a5ee7
syunta kitagawa <kitagawa@timedia.co.jp>
2013-06-27 11:37:09 +0900

    質問用にコミット

--- app.js
+++ app.js
@@ -68,7 +68,6 @@ function phaseChange(){
 /* 石を裏返す */
 function reverse(x,y){
 	
-	var count = 0;
 	var theBack = "";
 	
 	if(phase == "black"){
@@ -77,11 +76,50 @@ function reverse(x,y){
 		theBack = "●";
 	}
 	
-	/* 右方向反転 */
-	for(var i = 1; table.status[x+i][y] == theBack; i++){
-		count = i;
+	/* 反転チェック */
+	var num = 0;
+	var i = 1;
+	var test = "";
+	test = setDirection(x,y,num,i);
+	console.log(test);
+	
+	for(var num = 0; num < 4; num++){
+		
+		var count = reverseCheck(x,y,num,theBack);
+		
+		console.log(count);
+		
+		//for(var i = 0; i <= count; i++){
+		//	var reversedStone = setDirection(x,y,num,i) 
+		//	reversedStone = table.status[x][y];
+		//}
 	}
-	for(var i = 0; i <= count; i++){
-		table.status[x+i][y] = table.status[x][y];
-	}
+}
+
+function reverseCheck(x,y,num,theBack){
+	
+	var count = 0;
+	
+		for(var i = 0; setDirection(x,y,num,i) == theBack; i++){
+			
+			count++;
+			
+			if(table.status[x][y+i] != table.status[x][y]){
+				count = 0;
+			}
+		}
+	return count;
+}
+
+function setDirection(x,y,num,i){
+	var direction = [
+		table.status[x][y-i],
+		table.status[x+i][y],
+		table.status[x][y+i],
+		table.status[x-i][y]
+	];
+	
+	console.log(direction[num]);
+	
+	return direction[num];
 }
